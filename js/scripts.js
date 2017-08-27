8e7533d8ec820cd7baaf9bf30539f8cd25d55663
mo <meganannetteolson@yahoo.com>
2017-08-26 17:31:57 -0700

    print out correct number with class from html

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,34 @@ function Number(dob) {
       strings[i] = parseInt(strings[i]);
     }
     for (var i = 0, sum = 0; i <= strings.length; sum += strings[i++]){
-      var firstTotal = sum;
+      var number = sum;
     }
-    if (firstTotal > 9) {
-      var secondTotal = firstTotal.toString().split("");
-      for (var i = 0; i < secondTotal.length; i++) {
-        secondTotal[i] = parseInt(secondTotal[i]);
+    if (number > 9) {
+      number = number.toString().split("");
+      for (var i = 0; i < number.length; i++) {
+         number[i] = parseInt(number[i]);
       }
-      var finalTotal = secondTotal[0] + secondTotal[1];
-      return finalTotal;
-    } else {
-      return firstTotal;
+      number = number[0] + number[1];
+
+    }
+    if (number === 1) {
+      $(".one").show();
+    } else if (number === 2) {
+      $(".two").show();
+    } else if (number === 3) {
+      $(".three").show();
+    } else if (number === 4) {
+      $(".four").show();
+    } else if (number === 5) {
+      $(".five").show();
+    } else if (number === 6) {
+      $(".six").show();
+    } else if (number === 7) {
+      $(".seven").show();
+    } else if (number === 8) {
+      $(".eight").show();
+    } else if (number === 9){
+      $(".nine").show();
     }
   }
 }
@@ -29,10 +46,11 @@ $(document).ready(function(){
   $(".btn").click(function(e){
     e.preventDefault();
     var inputtedDob = $("input#dob-number").val();
-    // if (dob.length != 8) {
-    //   alert("Please enter your Date of Birth without any symbols and with all eight numbers before submitting.")
-    // }
+    if (inputtedDob.length != 8) {
+      alert("Please enter your Date of Birth without any symbols and with all eight numbers before submitting.")
+    } else {
     var result = new Number(inputtedDob);
     Number.prototype.calculateNumber();
+  }
   });
 });
